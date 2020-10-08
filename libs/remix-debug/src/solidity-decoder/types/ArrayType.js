bcc1df8f68db36c882dab290cb7fbd01cf4311ec
ioedeveloper <daviddisu8@gmail.com>
2020-10-08 15:02:29 +0100

    Set skip and limit for decode for memory internal

--- libs/remix-debug/src/solidity-decoder/types/ArrayType.js
+++ libs/remix-debug/src/solidity-decoder/types/ArrayType.js
@@ -76,7 +76,7 @@ class ArrayType extends RefType {
     }
   }
 
-  decodeFromMemoryInternal (offset, memory) {
+  decodeFromMemoryInternal (offset, memory, skip) {
     const ret = []
     let length = this.arraySize
     if (this.arraySize === 'dynamic') {
@@ -84,7 +84,17 @@ class ArrayType extends RefType {
       length = parseInt(length, 16)
       offset = offset + 32
     }
-    for (var k = 0; k < length; k++) {
+    if (isNaN(length)) {
+      return {
+        value: '<decoding failed - length is NaN>',
+        type: this.typeName
+      }
+    }
+    let limit = length
+    if (!skip) skip = 0
+    if (skip) offset = offset + (32 * skip)
+    if ((length - skip) > 500) limit = 500
+    for (var k = 0; k < limit; k++) {
       var contentOffset = offset
       ret.push(this.underlyingType.decodeFromMemory(contentOffset, memory))
       offset += 32
@@ -92,7 +102,9 @@ class ArrayType extends RefType {
     return {
       value: ret,
       length: '0x' + length.toString(16),
-      type: this.typeName
+      type: this.typeName,
+      cursor: skip + limit,
+      hasNext: length > (skip + limit)
     }
   }
 }
